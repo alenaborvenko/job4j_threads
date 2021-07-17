e84b5910273ada2a05bf11ee82d0b0b1c657e298
Borvenko Alena <alenaborvenko@gmail.com>
2021-07-17 17:24:48 +0700

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/io/ParseFile.java
@@ -0,0 +1,38 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.function.Predicate;
+
+public class ParseFile {
+    private final File file;
+
+    public ParseFile(File file) {
+        this.file = file;
+    }
+
+    public synchronized String getContent() throws IOException {
+        return getContentByPredicate((data) -> true);
+    }
+
+    private String getContentByPredicate(Predicate<Integer> condition) throws IOException {
+        StringBuilder str = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            int data;
+            while ((data = br.read()) > 0) {
+                if (condition.test(data)) {
+                    str.append((char) data);
+                }
+            }
+        }
+        return str.toString();
+    }
+
+    public synchronized String getContentWithoutUnicode() throws IOException {
+        return getContentByPredicate((data) -> data < 0x80);
+    }
+}
---
+++ src/main/java/io/SaveToFile.java
@@ -0,0 +1,18 @@
+package io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class SaveToFile {
+    private final File file;
+
+    public SaveToFile(File file) {
+        this.file = file;
+    }
+
+    public synchronized void saveContent(String content) throws IOException {
+        Files.writeString(Paths.get(file.toURI()), content);
+    }
+}
