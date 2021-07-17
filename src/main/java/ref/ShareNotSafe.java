b52b3fe4db48af311e67cc04aaa188fd0759cd21
Borvenko Alena <alenaborvenko@gmail.com>
2021-07-17 15:53:26 +0700

    4. Thread без общих ресурсов [#267919]

---
+++ src/main/java/ref/ShareNotSafe.java
@@ -0,0 +1,26 @@
+package ref;
+
+import java.util.List;
+
+public class ShareNotSafe {
+    public static void main(String[] args) throws InterruptedException {
+        UserCache userCache = new UserCache();
+        User user = User.of("name");
+        userCache.add(user);
+        userCache.add(User.of("new user"));
+        Thread first = new Thread(
+                () -> user.setName("rename")
+        );
+        Thread second = new Thread(
+                () -> {
+                    List<User> findAll = userCache.findAll();
+                    findAll.get(1).setName("rename two");
+                }
+        );
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        userCache.findAll().forEach(System.out::println);
+    }
+}
---
+++ src/main/java/ref/User.java
@@ -0,0 +1,39 @@
+package ref;
+
+import java.util.StringJoiner;
+
+public class User {
+    private int id;
+    private String name;
+
+    public static User of(String name) {
+        User user = new User();
+        user.name = name;
+        return user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", User.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("name='" + name + "'")
+                .toString();
+    }
+}
