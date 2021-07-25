bc402ccf633f139bc8d979f63f7779de32a315bc
Borvenko Alena <alenaborvenko@gmail.com>
2021-07-25 16:34:20 +0700

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/notify/SimpleBlockingQueueTest.java
+++ src/test/java/notify/SimpleBlockingQueueTest.java
@@ -2,6 +2,9 @@ package notify;
 
 import org.junit.Test;
 
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -32,5 +35,40 @@ public class SimpleBlockingQueueTest {
         producer.join();
         concumer.join();
         assertThat(10, is(queue.poll()));
+    }
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        Thread producer = new Thread(
+                () -> {
+                    for (int i = 0; i < 5; i++) {
+                        try {
+                            queue.offer(i);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(List.of(0, 1, 2, 3, 4)));
     }
 }
