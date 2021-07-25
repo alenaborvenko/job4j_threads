9be35ddb3b8036e4c3346ac6665c2cd8129c9013
Borvenko Alena <alenaborvenko@gmail.com>
2021-07-25 15:27:55 +0700

    3. Класс хранилища пользователей UserStorage [#1104 #178675]

--- src/main/java/userstorage/UserStorage.java
+++ src/main/java/userstorage/UserStorage.java
@@ -9,17 +9,17 @@ import java.util.Map;
 @ThreadSafe
 public class UserStorage implements Storage {
     @GuardedBy("this")
-    private final Map<Integer, Integer> userStorage = new HashMap<>();
+    private final Map<Integer, User> userStorage = new HashMap<>();
 
     @Override
     public synchronized boolean add(User user) {
-        userStorage.putIfAbsent(user.getId(), user.getAmount());
+        userStorage.putIfAbsent(user.getId(), user);
         return true;
     }
 
     @Override
     public synchronized boolean update(User user) {
-        return userStorage.put(user.getId(), user.getAmount()) != null;
+        return userStorage.put(user.getId(), user) != null;
     }
 
     @Override
@@ -29,12 +29,13 @@ public class UserStorage implements Storage {
 
     @Override
     public synchronized boolean transfer(int fromId, int toId, int amount) {
-        int amountFrom = userStorage.get(fromId);
-        if (amountFrom < amount || userStorage.get(toId) == null) {
+        User userFrom = userStorage.get(fromId);
+        User userTo = userStorage.get(toId);
+        if (userFrom == null || userFrom.getAmount() < amount || userTo == null) {
             return false;
         }
-        userStorage.put(fromId, amountFrom - amount);
-        userStorage.put(toId, userStorage.get(toId) + amount);
-        return true;
+        userFrom.setAmount(userFrom.getAmount() - amount);
+        userTo.setAmount(userTo.getAmount() + amount);
+        return update(userFrom) && update(userTo);
     }
 }
