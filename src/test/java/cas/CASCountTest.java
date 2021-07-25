42d1249a242d8d7cfd8fdb4d577a19f21365a5a7
Borvenko Alena <alenaborvenko@gmail.com>
2021-07-25 18:45:07 +0700

    0. CAS - операции [#6859 #181526]

---
+++ src/test/java/cas/CASCountTest.java
@@ -0,0 +1,21 @@
+package cas;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+
+public class CASCountTest {
+    @Test
+    public void casTest() throws InterruptedException {
+        CASCount count = new CASCount();
+        Thread th = new Thread(count::get);
+        Thread thread = new Thread(count::increment);
+        th.start();
+        thread.start();
+        th.join();
+        thread.join();
+        assertThat(count.get(), is(1));
+    }
+}
